0f4f0a58964688a83996597b85cc676b83b093e3
Nadezhda Hristova <epohster@gmail.com>
2023-04-12 16:03:10 +0300

    Init Vite React project

---
+++ random-quote-machine/src/App.tsx
@@ -0,0 +1,15 @@
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <div>
+        TODO
+      </div>
+    </div>
+  )
+}
+
+export default App
