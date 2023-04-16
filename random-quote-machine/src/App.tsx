516e49aa8c7d2495dd4ddda7752a52f6f68360a6
Nadezhda Hristova <epohster@gmail.com>
2023-04-16 23:03:56 +0300

    Added styles and svgs

--- random-quote-machine/src/App.tsx
+++ random-quote-machine/src/App.tsx
@@ -1,4 +1,7 @@
 import viteLogo from '/vite.svg'
+import twitter from './assets/twitter.svg';
+import facebook from './assets/facebook.svg';
+import octocat from './assets/octocat.svg';
 import './App.css'
 
 function App() {
@@ -8,8 +11,22 @@ function App() {
       <div id="quote-box">
         <div id="text">Text</div>
         <div id="author">Author</div>
-        <button id="new-quote"></button>
-        <a id="tweet-quote" href=""></a>
+        <div>
+        <a id="tweet-quote" href="">
+          <img src={twitter} alt="" />
+        </a>
+        <a id="facebook-quote" href="">
+          <img src={facebook} alt="" />
+        </a>
+          <button id="new-quote">New qote</button>
+        </div>
+      </div>
+      <div id="author-box">
+        by <a href="https://github.com/nhristova">
+          <img src={octocat} alt="" />
+          
+          nhristova
+          </a>
       </div>
     </div>
   )
